6a5f45b746950d2f32b939db78ecc9cbad4020bf
Eugene Dorian <pachendale@gmail.com>
2011-07-14 20:36:35 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rushreader::Application.config.secret_token = '1e61facbb762ef8cc3dbb2b82b91133193537f1eec016fc8aa71445ab739c651dbae5bcf0afa2409d495798d01cb8ecebe752ff27e1b848b5ad25096e589777b'
