dc6c75b5804c3c7aae05f1c6f8ddd9338bae8e10
Eugene Dorian <pachendale@gmail.com>
2011-07-20 17:41:42 +0300

    Работает ограничение по добавлению статей в день

--- app/models/article.rb
+++ app/models/article.rb
@@ -7,8 +7,13 @@ class Article < ActiveRecord::Base
 private
 
   def can_create_article
+
+    articles_quantity = APP_CONFIG['array1']
     today_articles = user.articles.where(:created_at => Time.now.midnight .. (Time.now.midnight + 1.day)).count
-    if today_articles <= user.setting.articles_quantity
+        logger.info "========================="
+logger.info "today_articles = #{today_articles} articles_quantity[user.setting.articles_quantity]= #{articles_quantity[user.setting.articles_quantity]}"
+logger.info "========================="
+    if today_articles < articles_quantity[user.setting.articles_quantity]
       return true
     else
       return false
