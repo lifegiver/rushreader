7c5a7f7bcf12b6e6e0b39dbb77587210db5aad26
Eugene Dorian <pachendale@gmail.com>
2011-08-01 20:15:51 +0300

    Commit for deploying

---
+++ config/deploy.rb
@@ -0,0 +1,44 @@
+# RVM
+
+$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
+require "rvm/capistrano"
+set :rvm_ruby_string, 'default'
+set :rvm_type, :user
+
+# Bundler
+
+require "bundler/capistrano"
+
+# General
+
+set :application, "rushreader"
+set :user, "dorian"
+
+set :deploy_to, "/home/#{user}/#{application}"
+set :deploy_via, :copy
+
+set :use_sudo, false
+
+# Git
+
+set :scm, :git
+set :repository,  "~/#{application}/.git"
+set :branch, "master"
+
+# VPS
+
+role :web, "178.79.173.254"
+role :app, "178.79.173.254"
+role :db,  "178.79.173.254", :primary => true
+role :db,  "178.79.173.254"
+
+# Passenger
+
+namespace :deploy do
+ task :start do ; end
+ task :stop do ; end
+ task :restart, :roles => :app, :except => { :no_release => true } do
+   run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+ end
+end
+
